f383c8ca5cc3d915c9f6717fb6dfa6f52857d18e
Your Name <you@example.com>
2024-03-08 11:21:18 +0100

    init

---
+++ scripts/practice3.7.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcloud compute firewall-rules create chat-service --allow=tcp:3000 --target-tag=chat
+
+gcloud compute instances create chat-vm --zone=europe-west1-b --custom-cpu=1 --custom-memory=4 --image=projects/ubuntu-os-pro-cloud/global/images/ubuntu-pro-2204-jammy-v20220923 --tags=chat --metadata startup-script-url=https://raw.githubusercontent.com/Rosstarz/sc-infra/main/startup.sh
