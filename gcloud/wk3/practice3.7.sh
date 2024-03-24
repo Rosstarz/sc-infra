7c6627cad5a5d12834045f403ff5302501c367d1
arti <56412092+Rosstarz@users.noreply.github.com>
2024-03-24 16:53:39 +0000

    adding rest

--- gcloud/wk3/practice3.7.sh
+++ gcloud/wk3/practice3.7.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create a firewall rule for TCP port 3000
 gcloud compute firewall-rules create chat-service --allow=tcp:3000 --target-tag=chat
 
 gcloud compute instances create chat-vm --zone=europe-west1-b --custom-cpu=1 --custom-memory=4 --image=projects/ubuntu-os-pro-cloud/global/images/ubuntu-pro-2204-jammy-v20220923 --tags=chat --metadata startup-script-url=https://raw.githubusercontent.com/Rosstarz/sc-infra/main/startup.sh
