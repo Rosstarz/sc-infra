a150c13a64f50a5d632171392d6734e3d35a577d
Ross <rostislavruckart@gmail.com>
2024-03-24 19:03:03 +0100

    added security assignments

---
+++ security/wk5/5.11_generate_certs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### ASSIGNMENT ###
+# 5.11 Generate personal certificate
+
+# Create a keypair and personal certificate using “openssl”.
+# Whereby of the certificate contains your name (CN, common name) and mail address (emailaddress). Other fields such as country, state, organization etc. can be freely chosen.
+
+# Required:
+# Use the command “openssl req”.
+# Keys and certificates must be in PEM format.
+
+###################
+
---
+++ security/wk5/5.12_sign_msg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### ASSIGNMENT ###
+# 5.12 Sign message
+# Sign a text message with your newly generated keypair.
+
+# Your signed message will be in S/MIME format, signed mail. But is a format heavily used for secure information exchange over multiple protocols.
+
+# S/MIME = secure MIME.
+# MIME   = Multipurpose Internet Mail Extensions
+
+# To avoid entering a during the signing process, pass the password as a shell variable:
+# export STUDENT=<your password>
+# openssl smime … -passin env:STUDENT
+
+# Required:
+# Use the “openssl smime” command to sign the message.
+# Put your name and email within the message.
+# The output must be a “MIME” message.
+# Your message must be readable/visible in the signed message.
+
+###################
